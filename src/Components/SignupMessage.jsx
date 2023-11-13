012d2301fde98a1f481717bd77b91f692e01a369
ysquareimperial <ysquare.theimperial@gmail.com>
2023-11-13 18:18:45 +0100

    save

--- src/Components/SignupMessage.jsx
+++ src/Components/SignupMessage.jsx
@@ -22,7 +22,7 @@ export default function Settings() {
         <Col md={4}>
           <div className="mt-3 text-center">
             <p className="m-0" style={{ fontWeight: "" }}>
-              Registration successful! Check your email for a confirmation link,
+              Registration successful! Check your email or span for a confirmation link,
               the link will be valid for only <b>1 hour</b> Welcome to wenyfour!
             </p>
           </div>
